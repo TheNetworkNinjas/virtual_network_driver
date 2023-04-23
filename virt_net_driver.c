223fb1c595be98bd0ce419d752478b300741aece
Craig Opie <craig@deathstardev.com>
2023-04-22 15:18:05 -1000

    added basic outline for our virt_net_driver

--- virt_net_driver.c
+++ virt_net_driver.c
@@ -4,6 +4,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include "virt_net_driver.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Craig Opie");
@@ -12,15 +13,154 @@ MODULE_AUTHOR("Lydia Sollis");
 MODULE_DESCRIPTION("Virtual network driver for Linux");
 MODULE_VERSION("0.01");
 
+static struct net_device *virt_net_dev;
+
 static int __init virt_net_driver_init(void)
 {
-	printk(KERN_ALERT "Hello, world \n");
-	return 0;
+    int ret;
+
+    /* Allocate and initialize net_device */
+    virt_net_dev = alloc_etherdev(sizeof(struct virt_net_dev_priv));
+    if (!virt_net_dev) {
+        printk(KERN_ERR "%s: Failed to allocate net_device\n", VIRT_NET_DRIVER_NAME);
+        return -ENOMEM;
+    }
+
+    /* Initialize net_device fields and operations */
+    // TODO: Fill in necessary fields and function pointers in virt_net_dev
+
+    /* Register the network device with the kernel */
+    ret = register_netdev(virt_net_dev);
+    if (ret) {
+        printk(KERN_ERR "%s: Failed to register net_device: %d\n", VIRT_NET_DRIVER_NAME, ret);
+        free_netdev(virt_net_dev);
+        return ret;
+    }
+
+    printk(KERN_INFO "%s: Virtual network driver loaded\n", VIRT_NET_DRIVER_NAME);
+    return 0;
 }
 
 static void __exit virt_net_driver_exit(void)
 {
-	printk(KERN_ALERT "bye bye");
+    /* Unregister the network device */
+    unregister_netdev(virt_net_dev);
+
+    /* Free the net_device memory */
+    free_netdev(virt_net_dev);
+
+    printk(KERN_INFO "%s: Virtual network driver unloaded\n", VIRT_NET_DRIVER_NAME);
+}
+
+static int virt_net_driver_open(struct net_device *dev)
+{
+    struct virt_net_dev_priv *priv = netdev_priv(dev);
+
+    /* Initialize any resources required for the virtual network driver */
+    // TODO: Initialize resources here
+
+    /* Start the network device's transmit queue */
+    netif_start_queue(dev);
+
+    printk(KERN_INFO "%s: Virtual network device opened\n", dev->name);
+
+    return 0;
+}
+
+static int virt_net_driver_stop(struct net_device *dev)
+{
+    /* Stop the network device's transmit queue */
+    netif_stop_queue(dev);
+
+    /* Cleanup any resources allocated for the virtual network driver */
+    // TODO: Cleanup resources here
+
+    printk(KERN_INFO "%s: Virtual network device closed\n", dev->name);
+
+    return 0;
+}
+
+static netdev_tx_t virt_net_driver_start_xmit(struct sk_buff *skb, struct net_device *dev)
+{
+    /* Process and transmit the packet using the virtual network driver */
+    // TODO: Transmit packet here
+
+    /* Update network device statistics */
+    dev->stats.tx_packets++;
+    dev->stats.tx_bytes += skb->len;
+
+    /* Free the skb */
+    dev_kfree_skb(skb);
+
+    return NETDEV_TX_OK;
+}
+
+static int virt_net_driver_set_mac_address(struct net_device *dev, void *addr)
+{
+    struct sockaddr *sa = addr;
+
+    /* Validate the new MAC address */
+    if (!is_valid_ether_addr(sa->sa_data)) {
+        printk(KERN_ERR "%s: Invalid MAC address\n", dev->name);
+        return -EADDRNOTAVAIL;
+    }
+
+    /* Update the device's MAC address */
+    memcpy(dev->dev_addr, sa->sa_data, dev->addr_len);
+
+    printk(KERN_INFO "%s: MAC address set to %pM\n", dev->name, dev->dev_addr);
+
+    return 0;
+}
+
+static int virt_net_driver_do_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
+{
+    int ret = -EOPNOTSUPP;
+
+    /* Handle custom ioctl commands for the virtual network driver */
+    switch (cmd) {
+        // case VIRT_NET_DRIVER_CUSTOM_CMD1:
+        //     ret = handle_custom_cmd1(dev, ifr);
+        //     break;
+        // case VIRT_NET_DRIVER_CUSTOM_CMD2:
+        //     ret = handle_custom_cmd2(dev, ifr);
+        //     break;
+        default:
+            printk(KERN_ERR "%s: Unsupported ioctl command: 0x%x\n", dev->name, cmd);
+            break;
+    }
+
+    return ret;
+}
+
+static int virt_net_driver_cfg80211_scan(struct wiphy *wiphy, struct cfg80211_scan_request *request)
+{
+    // TODO: Perform a Wi-Fi scan using the virtual network driver and report the scan results
+    // using cfg80211_scan_done()
+
+    printk(KERN_INFO "Virtual Wi-Fi scan initiated\n");
+
+    return 0;
+}
+
+static int virt_net_driver_cfg80211_connect(struct wiphy *wiphy, struct net_device *dev, struct cfg80211_connect_params *params)
+{
+    // TODO: Perform a Wi-Fi connection using the virtual network driver and report the connection
+    // status using cfg80211_connect_result()
+
+    printk(KERN_INFO "Virtual Wi-Fi connect initiated\n");
+
+    return 0;
+}
+
+static int virt_net_driver_cfg80211_disconnect(struct wiphy *wiphy, struct net_device *dev, u16 reason_code)
+{
+    // TODO: Perform a Wi-Fi disconnection using the virtual network driver and report the disconnection
+    // status using cfg80211_disconnected()
+
+    printk(KERN_INFO "Virtual Wi-Fi disconnect initiated\n");
+
+    return 0;
 }
 
 module_init(virt_net_driver_init);
