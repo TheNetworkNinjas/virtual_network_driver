09a3027c5d5a2d980a68a0345363e53f97a2d10e
Craig Opie <craig@deathstardev.com>
2023-04-23 02:36:43 -1000

    implemented the rx side of packet handling to complete the task.

--- virt_net_driver.h
+++ virt_net_driver.h
@@ -12,6 +12,7 @@
 #define VIRT_NET_DRIVER_NAME "virt_net_driver"
 #define VIRT_NET_DRIVER_VERSION "0.01"
 #define VIRT_FIFO_SIZE 4096
+#define VIRT_NET_DRIVER_MTU 1500
 
 // Virtual FIFO buffer for packet transmission
 struct virt_fifo {
@@ -39,11 +40,12 @@ static int init_virt_hw_resource(struct net_device *dev);
 static void release_virt_hw_resource(struct net_device *dev);
 
 /* Network Device Operations */
-static void virt_net_timer_callback(struct timer_list *t);
 static int virt_net_driver_open(struct net_device *dev);
 static int virt_net_driver_stop(struct net_device *dev);
 static void virt_net_tx_complete(struct net_device *dev, struct sk_buff *skb);
 static netdev_tx_t virt_net_driver_start_xmit(struct sk_buff *skb, struct net_device *dev);
+static void virt_net_rx_packet(struct net_device *dev, struct sk_buff *skb);
+static void virt_net_timer_callback(struct timer_list *t);
 static int virt_net_driver_set_mac_address(struct net_device *dev, void *addr);
 static int virt_net_driver_do_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd);
 
